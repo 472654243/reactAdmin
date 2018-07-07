ed9ab090a83d4d2a04c10af6572bb94ae1aeedde
dy <dingyi@360shop.cn>
2018-07-07 09:55:51 +0800

    1

---
+++ my-app/src/components/NavLeft/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Menu, Icon } from 'antd';
+import MenuConfig from './../../config/menuConfig'
+import './index.less'
+
+const SubMenu = Menu.SubMenu;
+
+export default class NavLeft extends React.Component { 
+  render() { 
+    return (
+      <div>NaVLeft</div>
+    );
+  }
+}
