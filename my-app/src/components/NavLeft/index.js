ddb258376c593406bdf67898216918fcf45bbcce
dy <dingyi@360shop.cn>
2018-07-07 10:56:20 +0800

    navleft

--- my-app/src/components/NavLeft/index.js
+++ my-app/src/components/NavLeft/index.js
@@ -6,9 +6,38 @@ import './index.less'
 const SubMenu = Menu.SubMenu;
 
 export default class NavLeft extends React.Component { 
+  componentWillMount(){ 
+    const menuTreeNode = this.renderMenu(MenuConfig)
+
+    this.setState({
+      menuTreeNode
+    })
+  }
+  renderMenu = (data) => { 
+    return data.map((item) => { 
+      if (item.children) { 
+        return (
+          <SubMenu key={item.key} title={<span><Icon type={item.icon} /><span>{item.title}</span></span>}>
+            { this.renderMenu(item.children)}
+          </SubMenu>
+        )
+      }
+      return (
+        <Menu.Item key={item.key}><Icon type={item.icon} />{item.title}</Menu.Item>        
+      )
+    })
+  }
   render() { 
     return (
-      <div>NaVLeft</div>
+      <div className="nav-left">
+        <div className="logo">
+            <img src="/assets/logo-ant.svg" alt=""/>
+            <h1>react Admin</h1>
+        </div>
+        <Menu>
+          { this.state.menuTreeNode }
+        </Menu>
+      </div>
     );
   }
 }
