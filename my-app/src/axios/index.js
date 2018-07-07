e6374fe7e59aee513fe62f1a27dd2604a93354e1
dy <dingyi@360shop.cn>
2018-07-07 14:22:27 +0800

    1

--- my-app/src/axios/index.js
+++ my-app/src/axios/index.js
@@ -5,8 +5,12 @@ export default class Axios {
     return new Promise((resolve,reject) => { 
       JsonP(options.url, {
         param:'callback'
-      }, function (err,response) { 
-
+      }, function (err, response) { 
+        if (response.status === 'success') {
+            resolve(response);
+        } else {
+            reject(response.message);
+        }
       })
     })
   }
--- my-app/src/components/Header/index.js
+++ my-app/src/components/Header/index.js
@@ -1,9 +1,59 @@
 import React from 'react'
+import Util from '../../utils/utils'
+import axios from '../../axios'
+import { Row,Col } from "antd"
+import './index.less'
+
 
 export default class Header extends React.Component { 
+  componentWillMount() { 
+    this.setState({
+      userName:'Dingyi'
+    })
+    setInterval(() => { 
+      let sysTime = Util.formateDate(new Date().getTime())
+      this.setState({
+        sysTime:sysTime
+      })
+    })
+    this.getWeatherAPIData()
+  }
+  getWeatherAPIData() { 
+    let city = '杭州';
+    axios.jsonp({
+      url:'http://api.map.baidu.com/telematics/v3/weather?location='+encodeURIComponent(city)+'&output=json&ak=3p49MVra6urFRGOT9s8UBWr2'
+    }).then((res) => { 
+      if(res.status === 'success'){
+        let data = res.results[0].weather_data[0];
+        this.setState({
+            dayPictureUrl:data.dayPictureUrl,
+            weather:data.weather
+        })
+      }
+    })
+  }
   render() { 
     return (
-      <div>Header</div>
+      <div className="header">
+        <Row className="header-top">
+          <Col span="24">
+            <span>欢迎，{this.state.userName}</span>
+            <a href="#">退出</a>
+          </Col>
+        </Row>
+        <Row className="breadcrumb">
+            <Col span="4" className="breadcrumb-title">首页</Col>
+            <Col span="20" className="weather">
+                <span className="date">{this.state.sysTime}</span>
+                <span className="weather-img">
+                    <img src={this.state.dayPictureUrl} alt="" />
+                </span>
+                <span className="weather-detail">
+                    {this.state.weather}
+                </span>
+            </Col>
+        </Row>
+      </div>
     );
   }
 }
