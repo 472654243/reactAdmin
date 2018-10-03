b9e2dfa374e2bdfb3c0901c770475f279676975e
dy <dingyi@360shop.cn>
2018-10-03 21:52:57 +0800

    table

--- my-app/src/router.js
+++ my-app/src/router.js
@@ -1,41 +1,48 @@
-import React from 'react'
-import { HashRouter, Route, Switch } from 'react-router-dom'
-import App from './App'
-import Admin from './admin'
-import Buttons from './pages/ui/buttons'
-import Modals from './pages/ui/modals'
-import Loadings from './pages/ui/loadings'
-import Notice from './pages/ui/notice'
-import Messages from './pages/ui/messages'
-import Tabs from './pages/ui/tabs'
-import Gallery from './pages/ui/gallery'
-import Carousel from './pages/ui/carousels'
-import FormLogin from './pages/form/login'
-import FormRegister from './pages/form/register'
+import React from 'react';
+import { HashRouter, Route, Switch } from 'react-router-dom';
+import App from './App';
+import Admin from './admin';
+import Buttons from './pages/ui/buttons';
+import Modals from './pages/ui/modals';
+import Loadings from './pages/ui/loadings';
+import Notice from './pages/ui/notice';
+import Messages from './pages/ui/messages';
+import Tabs from './pages/ui/tabs';
+import Gallery from './pages/ui/gallery';
+import Carousel from './pages/ui/carousels';
+import FormLogin from './pages/form/login';
+import FormRegister from './pages/form/register';
+import BasicTable from './pages/table/basicTable';
+import HighTable from './pages/table/highTable';
 
-export default class DRouter extends React.Component { 
-  render() { 
+export default class DRouter extends React.Component {
+  render() {
     return (
       <HashRouter>
         <App>
-          <Route path="/admin" render={() =>
-            <Admin>
-              <Switch>
-                <Route path='/admin/ui/buttons' component={Buttons} />
-                <Route path="/admin/ui/modals" component={Modals} />
-                <Route path="/admin/ui/loadings" component={Loadings} />
-                <Route path="/admin/ui/notification" component={Notice} />
-                <Route path="/admin/ui/messages" component={Messages} />
-                <Route path="/admin/ui/tabs" component={Tabs} />
-                <Route path="/admin/ui/gallery" component={Gallery} />
-                <Route path="/admin/ui/carousels" component={Carousel} />
-                <Route path="/admin/form/login" component={FormLogin} />
-                <Route path="/admin/form/reg" component={FormRegister} />
-              </Switch>
-            </Admin>
-          }/>
+          <Route
+            path="/admin"
+            render={() => (
+              <Admin>
+                <Switch>
+                  <Route path="/admin/ui/buttons" component={Buttons} />
+                  <Route path="/admin/ui/modals" component={Modals} />
+                  <Route path="/admin/ui/loadings" component={Loadings} />
+                  <Route path="/admin/ui/notification" component={Notice} />
+                  <Route path="/admin/ui/messages" component={Messages} />
+                  <Route path="/admin/ui/tabs" component={Tabs} />
+                  <Route path="/admin/ui/gallery" component={Gallery} />
+                  <Route path="/admin/ui/carousels" component={Carousel} />
+                  <Route path="/admin/form/login" component={FormLogin} />
+                  <Route path="/admin/form/reg" component={FormRegister} />
+                  <Route path="/admin/table/basic" component={BasicTable} />
+                  <Route path="/admin/table/high" component={HighTable} />
+                </Switch>
+              </Admin>
+            )}
+          />
         </App>
       </HashRouter>
-    )
+    );
   }
 }
